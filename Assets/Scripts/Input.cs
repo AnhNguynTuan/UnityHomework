d3750ef57e78872e15ba11e1a595f675ff4e412d
NguyenTuan <tuannguyenxxx.rex@gmail.com>
2026-01-12 18:18:03 +0700

    ImportKnight&Animation

---
+++ Assets/Scripts/Input.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Input : MonoBehaviour
+{
+    public KeyCode LeftKey;
+    public KeyCode RightKey;
+    public KeyCode UpKey;
+    public KeyCode DownKey;
+    public KeyCode CrouchKey;
+    public KeyCode AttackKey;
+    public KeyCode DashKey;
+    public KeyCode BlockKey;
+}
